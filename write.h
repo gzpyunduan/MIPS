13438f75e5328ae9d5a2910157e4e5dbf0bbea5b
gzpyunduan <38047636+gzpyunduan@users.noreply.github.com>
2018-07-10 16:56:05 +0800

    Update write.h

--- write.h
+++ write.h
@@ -1,13 +1,21 @@
 #ifndef _WRITE
 #define _WRITE
 
+#include "memory.h"
+
 void WriteBack(){
-  WB tmp = w.front();
-  if (w.empty()){
-    reg[tmp.rn] = tmp.ans1;
-    if (tmp.rn == 32) reg[33] = tmp.ans2;
+  if (w.empty()) return;
+  FF tmp = w.front();
+  if (tmp.rn == -1) return;
+  if (tmp.rn == 42) {
+	reg[31] = reg[34];
+	reg[34] = tmp.ans1;
   }
-  w.pop();
+  else {
+	reg[tmp.rn] = tmp.ans1;
+	if (tmp.rn == 32) reg[33] = tmp.ans2;
+  }
+  //w.pop();
 }
 
 #endif
