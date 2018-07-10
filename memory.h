4f244bb9e6dcc2aaca63d31f8879bd84b2814bbb
gzpyunduan <38047636+gzpyunduan@users.noreply.github.com>
2018-07-10 16:54:36 +0800

    Update memory.h

--- memory.h
+++ memory.h
@@ -1,25 +1,22 @@
 #ifndef _MEMORY
 #define _MEMORY
 
-#include "scanner.h"
-#include "parser.h"
 #include "performer.h"
 using namespace std;
 
-int CharToInt(int pos, int size){
-  int a;
-  for (int i = 0; i < size; i++)
-    a += Data[pos + i] << (i * 8);
-  return a;
-}
-
 void MemoryAccess(){
   if (m.empty()) return;
-  MA tmp = m.front();
-  if (tmp.type)
-    toChar(Data, tmp.address, tmp.size, reg[tmp.rn]);
-  else reg[tmp.rn] = CharToInt(tmp.address, tmp.size);
-  m.pop();
+  FF tmp = m.front();
+  if (tmp.t || tmp.rn == -1) w.push(tmp);
+  else {
+    if (tmp.type)
+	  toChar(tmp.address, tmp.size, reg[tmp.rn]);
+    else reg[tmp.rn] = CharToInt(tmp.address, tmp.size);
+	FF tmp1;
+	w.push(tmp1);
+  }
+  
+  //m.pop();
 }
 
 #endif
