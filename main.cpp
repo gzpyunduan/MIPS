f1850a956ab2b2d5936ab258bf6fb6efe4c733ae
gzpyunduan <38047636+gzpyunduan@users.noreply.github.com>
2018-07-11 09:52:28 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,27 +11,28 @@
 #include "write.h"
 using namespace std;
 
-int main(int argc, char *argv[]){
-  //freopen("19.s", "r", stdin);
-  //freopen("1.out", "w", stdout);
-  fin.open(argv[1]);
-  preProcess();
-  //freopen("19.in", "r", stdin);
-  while(true){
-	s = ""; t.tk = NONE;
-	if (!m.empty()) m.pop();
-	if (!w.empty()) w.pop();
+int main(int argc, char *argv[]) {
+	freopen("1.s", "r", stdin);
+	//freopen("1.out", "w", stdout);
+	//fin.open(argv[1]);
+	preProcess();
+	freopen("1.in", "r", stdin);
 
-	//cout << reg[34] << endl;
-    InstructionFetch();
-    InstructionDecode();
-	Execute();
-    MemoryAccess();
-	WriteBack();
-    
-    if (s == "\0" && t.tk == NONE && m.empty() && w.empty())
-      break;
-  }
-  while(true){}
-  return 0;
+	while (true) {
+		s = ""; t.tk = NONE;
+		if (!m.empty()) m.pop();
+		if (!w.empty()) w.pop();
+
+		//cout << reg[34] << endl;
+		InstructionFetch();
+		InstructionDecode();
+		Execute();
+		MemoryAccess();
+		WriteBack();
+
+		if (s == "\0" && t.tk == NONE && m.empty() && w.empty())
+			break;
+	}
+	while (true) {}
+	return 0;
 }
